e4a725e80e4428107fb231cf12a8071740a7f601
jmoses89 <jarret.moses@gmail.com>
2014-11-21 15:13:49 -0500

    create login check and page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create, :new]
+  
+  def new
+    
+  end
+
   def create
     @user = User.login_from_omniauth(auth_hash)
     log_in(@user)
