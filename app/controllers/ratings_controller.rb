18ee9af75a9f55e4fcef8155ed6ada2babfc2280
changamanda <amanda.chang10@gmail.com>
2014-11-21 10:04:09 -0500

    Buttons

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,27 @@
 class RatingsController < ApplicationController
   def create
-    Rating.create(user_id: current_user.id, movie_id: params[:movie_id], score: params[:score])
-    
+    Rating.create(user_id: current_user.id, movie_id: params[:movie_id], score: params[:score])    
     @movie = Movie.pick_movie
   end
 
   def index
     @ratings_yes = Rating.where(user_id: current_user.id, score: 2, watched: 0)
     @ratings_maybe = Rating.where(user_id: current_user.id, score: 1, watched: 0)
+  end
+
+  def update
+  	rating = Rating.find(params[:id])
+  	rating.watched = true
+  	rating.save
+
+  	redirect_to ratings_path
+  end
+
+  def destroy
+  	rating = Rating.find(params[:id])
+  	rating.destroy
+
+  	redirect_to ratings_path
   end
 
 end
