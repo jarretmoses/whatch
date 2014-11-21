abf858f7e763bafc5dac7047add07990a80650f6
changamanda <amanda.chang10@gmail.com>
2014-11-21 09:20:24 -0500

    Refactor

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -1,15 +1,15 @@
-$(function(){
-  $('.button_to').on('submit',function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    // What type of request should I fire and where should I fire it to?
-    var form_method = $(this).attr("method");
-    var form_action = $(this).attr("action");
-    // Fire the Request
-    $.ajax(form_action, {
-      type: form_method,
-      data: $(this).serialize(),
-      dataType: "script"
-    });
-  });
-});
+// $(function(){
+//   $('.button_to').on('submit',function(e){
+//     e.preventDefault();
+//     e.stopPropagation();
+//     // What type of request should I fire and where should I fire it to?
+//     var form_method = $(this).attr("method");
+//     var form_action = $(this).attr("action");
+//     // Fire the Request
+//     $.ajax(form_action, {
+//       type: form_method,
+//       data: $(this).serialize(),
+//       dataType: "script"
+//     });
+//   });
+// });
