79e76f2d06cec0468510a28060fa589417d4da39
jmoses89 <jarret.moses@gmail.com>
2014-11-24 17:10:51 -0500

    move genre filter js into asset pipeline

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -13,3 +13,25 @@
 //     });
 //   });
 // });
+
+$(function(){
+  
+  var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+      showLeft = document.getElementById( 'showLeft' ),
+      body = document.body;
+   
+  showLeft.onclick = function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      classie.toggle( this, 'active' );
+      classie.toggle( menuLeft, 'cbp-spmenu-open' );
+      disableOther( 'showLeft' );
+  };
+
+   
+  function disableOther( button ) {
+      if( button !== 'showLeft' ) {
+          classie.toggle( showLeft, 'disabled' );
+      }
+  }
+});
