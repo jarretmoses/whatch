74c0756bda5ad8316900e8ecccceced1de727aa0
jmoses89 <jarret.moses@gmail.com>
2014-11-24 21:28:02 -0500

    add twitter gem and tweet capability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :require_login
+  before_action :require_login, :set_client
  
   private
 
@@ -22,12 +22,25 @@ class ApplicationController < ActionController::Base
 
 		def log_out
 	    session.delete(:user_id)
+      session.delete(:token)
+      session.delete(:secret)
 	    @current_user = nil
 	  end
 
 	  def current_user
 		  @current_user ||= User.find_by(id: session[:user_id])
 		end
+
+    def set_client
+      if logged_in?
+        @client = Twitter::REST::Client.new do |config|
+          config.consumer_key        = ENV['TWITTER_KEY']
+          config.consumer_secret     = ENV['TWITTER_SECRET']
+          config.access_token        = session[:token]
+          config.access_token_secret = session[:secret]
+        end
+      end
+    end
 		helper_method :current_user
 
 end
