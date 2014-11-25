a69814dcac2c5945e09418dc376327cb37a9651d
changamanda <amanda.chang10@gmail.com>
2014-11-25 16:29:54 -0500

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {:host => "https://whatch.herokuapp.com/"}
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :enable_starttls_auto => true,
+     :user_name            => ENV['EMAIL_LOGIN'],
+     :password             => ENV['EMAIL_PASSWORD'],
+     :domain               => 'gmail.com',
+     :authentication       => 'plain'
+  }
 end
